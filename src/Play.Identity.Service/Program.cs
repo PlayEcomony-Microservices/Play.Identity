561bb27f77ca1bed1f3d3e271a3556aee6431ebf
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-03-02 22:22:00 +0300

    add dotnet core identity scaffold

--- src/Play.Identity.Service/Program.cs
+++ src/Play.Identity.Service/Program.cs
@@ -2,8 +2,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("IdentityDataContextConnection") ?? throw new InvalidOperationException("Connection string 'IdentityDataContextConnection' not found.");
+
+// builder.Services.AddDbContext<IdentityDataContext>(options => options.UseSqlServer(connectionString));
+
+// builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<IdentityDataContext>();
 
 var services = builder.Services;
 
